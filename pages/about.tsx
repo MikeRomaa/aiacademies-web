d6e5debf75a219ac68dd68451256bc96ae0a7c96
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2023-08-08 01:34:41 -0400

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,7 +10,7 @@ const About: NextPage = () => (
     <>
         <PageHeader title="About Us" />
         <div className="container py-10">
-            <h1>Founders</h1>
+            <h1>Our Team</h1>
             <br/>
             <div className="lg:flex gap-10 mb-5">
                 <div>
@@ -18,7 +18,7 @@ const About: NextPage = () => (
                     <Image src={rayhan} alt="Rayhan Zirvi" layout="fixed" width={598 / 4} height={676 / 4} />
                 </div>
                 <div>
-                    <h3>Rayhan Zirvi</h3>
+                    <h3>Rayhan Zirvi, Founder and CEO</h3>
                     <p>Rayhan Zirvi is an incoming freshman at Caltech. He is passionate about helping others learn about computer science and AI. He has qualified for the USACO Platinum division, reached Master on Codeforces, qualified for AIME twice, and placed 11th in the US at Google Kick Start. He has partnered with <a className="underline text-blue-600" href="https://theeduproject.net/" target="_blank" rel="noreferrer">The Education Project By Humanity First USA</a> and <a className="underline text-blue-600" href="https://aa.academy/" target="_blank" rel="noreferrer">Ayesha Ali Academy</a> to develop their AI curriculum and teach through instructional web seminars. He also served as a director for <a className="underline text-blue-600" href="https://hocohoc.com/" target="_blank" rel="noreferrer">Howard County Hour of Code</a>, where he raised over $20,000 in sponsorships, directed content, and outreached to schools. In his free time, he enjoys watching the NBA, playing basketball, and playing golf.</p>
                 </div>
             </div>
@@ -28,7 +28,7 @@ const About: NextPage = () => (
                     <Image src={saketh} alt="Saketh Sundar" layout="fixed" width={700 / 4} height={676 / 4} />
                 </div>
                 <div>
-                    <h3>Saketh Sundar</h3>
+                    <h3>Saketh Sundar, Founder</h3>
                     <p>Saketh Sundar is an incoming freshman at Harvard interested in the intersection of computational sciences with other fields within STEM. He is involved in multiple STEM extracurricular activities at school and holds leadership positions in Science Olympiad, Maryland Technology Honor Society, and AI Club. His research interests are in applying machine learning methods to environmental and medical science problems, and he has pursued these through a variety of different research internships. In his free time, Saketh enjoys watching football and movies.</p>
                 </div>
             </div>
@@ -38,8 +38,8 @@ const About: NextPage = () => (
                     <Image src={nathan} alt="Nathan Zhong" layout="fixed" width={800 / 4} height={676 / 4} />
                 </div>
                 <div>
-                    <h3>Nathan Zhong</h3>
-                    <p>Saketh Sundar is an incoming freshman at Harvard interested in the intersection of computational sciences with other fields within STEM. He is involved in multiple STEM extracurricular activities at school and holds leadership positions in Science Olympiad, Maryland Technology Honor Society, and AI Club. His research interests are in applying machine learning methods to environmental and medical science problems, and he has pursued these through a variety of different research internships. In his free time, Saketh enjoys watching football and movies.</p>
+                    <h3>Nathan Zhong, Outreach Director</h3>
+                    <p>Nathan Zhong is a junior at River Hill High School. He is the Co-President of his school's programming club and co-coaches his middle school math team. He enjoys math contests and programming contests and is currently trying to free himself from hardstuck USACO silver and break out of his trend of exponentially decaying AIME scores. Nathan has also participated in a variety of community service events and nonprofits, hoping to utilize his experiences to better promote passion for AI in the community. In his free time, Nathan loves playing basketball and exploring the intersections between AI and public health.</p>
                 </div>
             </div>
         </div>
