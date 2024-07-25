426c6aa92171246ba972f622248a47f2001f8398
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-25 18:03:14 -0400

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -22,8 +22,9 @@ const Blog: NextPage = () => (
             <a href="https://news.mit.edu/2024/study-structured-randomization-ai-can-improve-fairness-0724" target="_blank" rel="noreferrer">
                 <a className="no-underline">
                     <Card className="!p-0 duration-500 hover:-translate-y-2">
-                        <Image src={Article} alt="Article Image" layout="fixed" width={700 / 4} height={676 / 4} />
+                        <Image src={Article} alt="Article Image" layout="fixed" width={700} height={676} />
                         <div className="p-4">
+                            <h4>{"Study: When allocating scarce resources with AI, randomization can improve fairness"}</h4>
                             <p className="inline-flex items-center text-slate-400">
                                 <RiFileCopy2Line size={22} className="mr-2" />
                             </p>
