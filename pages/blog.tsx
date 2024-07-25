3fdcf3ce3d3a7a7d9bb7710a0ab6a8eee990c28e
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-25 17:39:24 -0400

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -11,59 +11,6 @@ import Image from 'next/image';
 interface CourseCardProps {
     course: BaseCourse;
 }
-const util = require("util");
-const request = util.promisify(require("request"));
-const getUrls = require("get-urls");
-
-const urlImageIsAccessible = async https://news.mit.edu/2024/study-structured-randomization-ai-can-improve-fairness-0724 => {
-  const correctedUrls = getUrls(url);
-  if (correctedUrls.size !== 0) {
-    const urlResponse = await request(correctedUrls.values().next().value);
-    const contentType = urlResponse.headers["content-type"];
-    return new RegExp("image/*").test(contentType);
-  }
-};
-
-const getImg = async (page, uri) => {
-  const img = await page.evaluate(async () => {
-    const ogImg = document.querySelector('meta[property="og:image"]');
-    if (
-      ogImg != null &&
-      ogImg.content.length > 0 &&
-      (await urlImageIsAccessible(ogImg.content))
-    ) {
-      return ogImg.content;
-    }
-    let imgs = Array.from(document.getElementsByTagName("img"));
-    if (imgs.length > 0) {
-      imgs = imgs.filter(img => {
-        let addImg = true;
-        if (img.naturalWidth > img.naturalHeight) {
-          if (img.naturalWidth / img.naturalHeight > 3) {
-            addImg = false;
-          }
-        } else {
-          if (img.naturalHeight / img.naturalWidth > 3) {
-            addImg = false;
-          }
-        }
-        if (img.naturalHeight <= 50 || img.naturalWidth <= 50) {
-          addImg = false;
-        }
-        return addImg;
-      });
-      imgs.forEach(img =>
-        img.src.indexOf("//") === -1
-          ? (img.src = `${new URL(uri).origin}/${src}`)
-          : img.src
-      );
-      return imgs[0].src;
-    }
-    return null;
-  });
-  return img;
-};
-
 const Blog: NextPage = () => (
     <>
         <PageHeader title="Blog" />
@@ -73,10 +20,7 @@ const Blog: NextPage = () => (
             <a href="https://news.mit.edu/2024/study-structured-randomization-ai-can-improve-fairness-0724" target="_blank" rel="noreferrer">
                 <a className="no-underline">
                     <Card className="!p-0 duration-500 hover:-translate-y-2">
-                        <img
-                            src={getImg}
-                            className="h-60 w-full object-cover rounded-t-2xl"
-                        />
+                        <Image src={saketh} alt="Saketh Sundar" layout="fixed" width={700 / 4} height={676 / 4} />
                         <div className="p-4">
                             <p className="inline-flex items-center text-slate-400">
                                 <RiFileCopy2Line size={22} className="mr-2" />
