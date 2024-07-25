094b9fdc8f9a82fe36bd0778cc38370aa6a981ae
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-25 17:02:13 -0400

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -23,15 +23,11 @@ const Blog: NextPage = () => (
         <a className="no-underline">
             <Card className="!p-0 duration-500 hover:-translate-y-2">
                 <img
-                    src={course.banner}
-                    alt={course.name}
                     className="h-60 w-full object-cover rounded-t-2xl"
                 />
                 <div className="p-4">
-                    <h4>{course.name}</h4>
                     <p className="inline-flex items-center text-slate-400">
                         <RiFileCopy2Line size={22} className="mr-2" />
-                        {course.num_lessons} Lesson{course.num_lessons === 1 ? '' : 's'}
                     </p>
                 </div>
             </Card>
