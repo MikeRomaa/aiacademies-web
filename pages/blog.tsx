016a5e3b06c79673d220b70d2cbcc5a5fcd3834c
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-25 16:51:33 -0400

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -13,26 +13,28 @@ const Blog: NextPage = () => (
     <>
         <PageHeader title="Blog" />
         <div className="container py-10">
-            <h1>This weeks AI News</h1>
-            <br/>
-            <div className="lg:flex gap-10 mb-5">
+        <h1>This weeks AI News</h1>
+        <br/>
+        <Link href={`/courses/${course.id}`} passHref>
+        <a className="no-underline">
+            <Card className="!p-0 duration-500 hover:-translate-y-2">
+                <img
+                    src={course.banner}
+                    alt={course.name}
+                    className="h-60 w-full object-cover rounded-t-2xl"
+                />
+                <div className="p-4">
+                    <h4>{course.name}</h4>
+                    <p className="inline-flex items-center text-slate-400">
+                        <RiFileCopy2Line size={22} className="mr-2" />
+                        {course.num_lessons} Lesson{course.num_lessons === 1 ? '' : 's'}
+                    </p>
+                </div>
+            </Card>
+        </a>
+    </Link>
 
-            </div>
-            <div className="lg:flex gap-10 mb-5">
 
-            </div>
-            <div className="lg:flex gap-10 mb-5">
-
-            </div>
-            <div className="lg:flex gap-10 mb-5">
-
-            </div>
-            <div className="lg:flex gap-10 mb-5">
-
-            </div>
-            <div className="lg:flex gap-10 mb-5">
-
-            </div>
             <h2>Updates & Info</h2>
         </div>
     </>
