7eac2aa0e33c5bfffe239d9d11f56597b7140390
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-25 17:06:00 -0400

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -17,21 +17,19 @@ const Blog: NextPage = () => (
     <>
         <PageHeader title="Blog" />
         <div className="container py-10">
-        <h1>This weeks AI News</h1>
-        <br/>
-        <a href="https://discord.gg/XSn8nxqvw4" target="_blank" rel="noreferrer">
-        <a className="no-underline">
-            <Card className="!p-0 duration-500 hover:-translate-y-2">
-                <div className="p-4">
-                    <p className="inline-flex items-center text-slate-400">
-                        <RiFileCopy2Line size={22} className="mr-2" />
-                    </p>
-                </div>
-            </Card>
-        </a>
-    </a>
-
-
+            <h1>This weeks AI News</h1>
+            <br/>
+            <a href="https://discord.gg/XSn8nxqvw4" target="_blank" rel="noreferrer">
+                <a className="no-underline">
+                    <Card className="!p-0 duration-500 hover:-translate-y-2">
+                        <div className="p-4">
+                            <p className="inline-flex items-center text-slate-400">
+                                <RiFileCopy2Line size={22} className="mr-2" />
+                            </p>
+                        </div>
+                    </Card>
+                </a>
+            </a>
             <h2>Updates & Info</h2>
         </div>
     </>
