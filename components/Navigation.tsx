62a7fb1e4d3f919cf2785537abbd968db21506c9
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-26 15:38:50 -0400

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -16,7 +16,7 @@ const Navigation: React.FC = () => {
     return (
         <nav className="sticky top-0 bg-white z-30 py-4">
             <div className="container flex items-center justify-between gap-4 lg:gap-8">
-                {/* Hamburger Menu */}
+                {/* Hamburger Menu Button */}
                 <button
                     className="lg:hidden flex items-center"
                     onClick={toggleMenu}
@@ -37,28 +37,26 @@ const Navigation: React.FC = () => {
                         />
                     </a>
                 </Link>
-                {/* Navigation Links */}
-                <div className={`lg:flex lg:ml-auto ${isMenuOpen ? 'flex' : 'hidden'} lg:gap-8`}>
-                    <div className="flex-col lg:flex-row flex items-center gap-4 lg:gap-8">
-                        <Link href="/" passHref>
-                            <a className="py-2 lg:py-0">Home</a>
-                        </Link>
-                        <Link href="/courses" passHref>
-                            <a className="py-2 lg:py-0">Courses</a>
-                        </Link>
-                        <Link href="/about" passHref>
-                            <a className="py-2 lg:py-0">About Us</a>
-                        </Link>
-                        <Link href="/partners" passHref>
-                            <a className="py-2 lg:py-0">Partners</a>
-                        </Link>
-                        <Link href="/blog" passHref>
-                            <a className="py-2 lg:py-0">Blog</a>
-                        </Link>
-                    </div>
+                {/* Navigation Links for larger screens */}
+                <div className="hidden lg:flex lg:ml-auto lg:gap-8">
+                    <Link href="/" passHref>
+                        <a className="py-2">Home</a>
+                    </Link>
+                    <Link href="/courses" passHref>
+                        <a className="py-2">Courses</a>
+                    </Link>
+                    <Link href="/about" passHref>
+                        <a className="py-2">About Us</a>
+                    </Link>
+                    <Link href="/partners" passHref>
+                        <a className="py-2">Partners</a>
+                    </Link>
+                    <Link href="/blog" passHref>
+                        <a className="py-2">Blog</a>
+                    </Link>
                 </div>
                 {/* Sign In/Sign Out and Session Info */}
-                <div className="flex items-center gap-4 ml-auto">
+                <div className="hidden lg:flex items-center gap-4 ml-auto">
                     {session ? (
                         <>
                             <p className="font-medium hidden lg:block">Signed in as {session.fullName}</p>
@@ -86,6 +84,58 @@ const Navigation: React.FC = () => {
                             </Link>
                         </>
                     )}
+                </div>
+            </div>
+            {/* Off-Canvas Menu */}
+            <div 
+                className={`fixed inset-0 bg-gray-800 bg-opacity-75 z-40 transition-transform transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} lg:hidden`}
+                onClick={toggleMenu}
+            >
+                <div className="flex flex-col items-center justify-center h-full">
+                    <button className="absolute top-4 right-4 text-white" onClick={toggleMenu}>
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                    <div className="flex flex-col items-center space-y-4">
+                        <Link href="/" passHref>
+                            <a className="text-white text-xl" onClick={toggleMenu}>Home</a>
+                        </Link>
+                        <Link href="/courses" passHref>
+                            <a className="text-white text-xl" onClick={toggleMenu}>Courses</a>
+                        </Link>
+                        <Link href="/about" passHref>
+                            <a className="text-white text-xl" onClick={toggleMenu}>About Us</a>
+                        </Link>
+                        <Link href="/partners" passHref>
+                            <a className="text-white text-xl" onClick={toggleMenu}>Partners</a>
+                        </Link>
+                        <Link href="/blog" passHref>
+                            <a className="text-white text-xl" onClick={toggleMenu}>Blog</a>
+                        </Link>
+                        {session ? (
+                            <Link href="/signout" passHref>
+                                <a>
+                                    <Button className="bg-deepblue-500 text-white font-medium" onClick={toggleMenu}>
+                                        Sign Out
+                                    </Button>
+                                </a>
+                            </Link>
+                        ) : (
+                            <>
+                                <Link href="/signin" passHref>
+                                    <a className="text-white text-xl" onClick={toggleMenu}>Sign In</a>
+                                </Link>
+                                <Link href="/signup" passHref>
+                                    <a>
+                                        <Button className="bg-deepblue-500 text-white font-medium" onClick={toggleMenu}>
+                                            Sign Up
+                                        </Button>
+                                    </a>
+                                </Link>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
         </nav>
