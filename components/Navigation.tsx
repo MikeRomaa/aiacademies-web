5bb07d812187079e22b7fc29bcc74d4a28339f24
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-26 15:45:04 -0400

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -15,30 +15,32 @@ const Navigation: React.FC = () => {
     };
     return (
         <nav className="sticky top-0 bg-white z-30 py-4">
-            <div className="container flex items-center justify-between gap-4 lg:gap-8">
+            <div className="container flex items-center justify-between gap-4 lg:gap-8 px-4 lg:px-0">
                 {/* Hamburger Menu Button */}
                 <button
-                    className="lg:hidden flex items-center"
+                    className="lg:hidden flex items-center text-2xl text-gray-900"
                     onClick={toggleMenu}
                 >
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                     </svg>
                 </button>
+                
                 {/* Logo */}
                 <Link href="/" passHref>
                     <a className="flex-shrink-0">
                         <Image
                             src={logo}
                             alt="AI Academies"
-                            height={100}
-                            width={333}
+                            height={60}
+                            width={200}
                             className="w-auto h-16 lg:h-24"
                         />
                     </a>
                 </Link>
+                
                 {/* Navigation Links for larger screens */}
-                <div className="hidden lg:flex lg:ml-auto lg:gap-8">
+                <div className="hidden lg:flex lg:ml-auto lg:gap-8 items-center">
                     <Link href="/" passHref>
                         <a className="py-2">Home</a>
                     </Link>
@@ -55,6 +57,7 @@ const Navigation: React.FC = () => {
                         <a className="py-2">Blog</a>
                     </Link>
                 </div>
+                
                 {/* Sign In/Sign Out and Session Info */}
                 <div className="hidden lg:flex items-center gap-4 ml-auto">
                     {session ? (
@@ -86,32 +89,33 @@ const Navigation: React.FC = () => {
                     )}
                 </div>
             </div>
+
             {/* Off-Canvas Menu */}
-            <div
+            <div 
                 className={`fixed inset-0 bg-gray-800 bg-opacity-75 z-40 transition-transform transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} lg:hidden`}
                 onClick={toggleMenu}
             >
                 <div className="flex flex-col items-center justify-center h-full">
-                    <button className="absolute top-4 right-4 text-white" onClick={toggleMenu}>
-                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <button className="absolute top-4 right-4 text-white text-2xl" onClick={toggleMenu}>
+                        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
                     <div className="flex flex-col items-center space-y-4">
                         <Link href="/" passHref>
-                            <a className="text-white text-xl" onClick={toggleMenu}>Home</a>
+                            <a className="text-white text-2xl" onClick={toggleMenu}>Home</a>
                         </Link>
                         <Link href="/courses" passHref>
-                            <a className="text-white text-xl" onClick={toggleMenu}>Courses</a>
+                            <a className="text-white text-2xl" onClick={toggleMenu}>Courses</a>
                         </Link>
                         <Link href="/about" passHref>
-                            <a className="text-white text-xl" onClick={toggleMenu}>About Us</a>
+                            <a className="text-white text-2xl" onClick={toggleMenu}>About Us</a>
                         </Link>
                         <Link href="/partners" passHref>
-                            <a className="text-white text-xl" onClick={toggleMenu}>Partners</a>
+                            <a className="text-white text-2xl" onClick={toggleMenu}>Partners</a>
                         </Link>
                         <Link href="/blog" passHref>
-                            <a className="text-white text-xl" onClick={toggleMenu}>Blog</a>
+                            <a className="text-white text-2xl" onClick={toggleMenu}>Blog</a>
                         </Link>
                         {session ? (
                             <Link href="/signout" passHref>
@@ -124,7 +128,7 @@ const Navigation: React.FC = () => {
                         ) : (
                             <>
                                 <Link href="/signin" passHref>
-                                    <a className="text-white text-xl" onClick={toggleMenu}>Sign In</a>
+                                    <a className="text-white text-2xl" onClick={toggleMenu}>Sign In</a>
                                 </Link>
                                 <Link href="/signup" passHref>
                                     <a>
