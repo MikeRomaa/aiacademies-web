c99160d0cd9573090dfc462fd9ca8a1217d47bda
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-08-01 21:00:07 -0400

    Update LessonLink.tsx

--- components/LessonLink.tsx
+++ components/LessonLink.tsx
@@ -7,13 +7,12 @@ import { useSession } from '~/utils/sessionHooks';
 interface LessonLinkProps {
     course: Course;
     lesson: Lesson;
-    isCompleted: boolean; // Add this prop
 }
 
-export const LessonLink: React.FC<LessonLinkProps> = ({ course, lesson, isCompleted }) => (
+export const LessonLink: React.FC<LessonLinkProps> = ({ course, lesson }) => (
     <Link href={`/courses/${course.id}/lesson/${lesson.id}`} passHref>
         <a className="relative flex flex-col lg:flex-row justify-between duration-200 hover:bg-slate-100 rounded px-4 py-3">
-            <span className={`absolute -left-8 top-4 w-6 h-6 rounded-full ${isCompleted ? 'bg-green-500' : 'bg-slate-200'}`} />
+            <span className="absolute -left-8 top-4 w-6 h-6 bg-slate-200 border-white border-4 rounded-full" />
             <div>
                 <h5 className="font-medium mb-1">{lesson.number ?? 0}. {lesson.title}</h5>
                 <div className="text-slate-400">
@@ -37,16 +36,15 @@ export const LessonLink: React.FC<LessonLinkProps> = ({ course, lesson, isComple
 interface QuizLinkProps {
     course: Course;
     quiz: Quiz;
-    isCompleted: boolean; // Add this prop
 }
 
-export const QuizLink: React.FC<QuizLinkProps> = ({ course, quiz, isCompleted }) => {
+export const QuizLink: React.FC<QuizLinkProps> = ({ course, quiz }) => {
     const session = useSession();
     if (session) {
         return (
             <Link href={`/courses/${course.id}/quiz/${quiz.id}`} passHref>
-                <a className="relative flex flex-col lg:flex-row justify-between duration-200 hover:bg-slate-100 rounded px-4 py-3">
-                    <span className={`absolute -left-8 top-4 w-6 h-6 rounded-full ${isCompleted ? 'bg-green-500' : 'bg-slate-200'}`} />
+                <a className="relative flex flex-col lg:flex-row  justify-between duration-200 hover:bg-slate-100 rounded px-4 py-3">
+                    <span className="absolute -left-8 top-4 w-6 h-6 bg-slate-200 border-white border-4 rounded-full" />
                     <div>
                         <h5 className="font-medium mb-0">{quiz.number ?? 0}. {quiz.title}</h5>
                     </div>
@@ -58,8 +56,8 @@ export const QuizLink: React.FC<QuizLinkProps> = ({ course, quiz, isCompleted })
         );
     }
     return (
-        <a className="relative flex flex-col lg:flex-row justify-between rounded px-4 py-3">
-            <span className={`absolute -left-8 top-4 w-6 h-6 rounded-full ${isCompleted ? 'bg-green-500' : 'bg-slate-200'}`} />
+        <a className="relative flex flex-col lg:flex-row  justify-between rounded px-4 py-3">
+            <span className="absolute -left-8 top-4 w-6 h-6 bg-slate-200 border-white border-4 rounded-full" />
             <div>
                 <h5 className="font-medium mb-0">{quiz.number ?? 0}. {quiz.title}</h5>
             </div>
