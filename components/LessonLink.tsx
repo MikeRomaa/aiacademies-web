40b1e4cad630ca73cd9084d93a99f4099d1f188c
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-09-30 22:36:44 -0400

    Update LessonLink.tsx

--- components/LessonLink.tsx
+++ components/LessonLink.tsx
@@ -1,36 +1,17 @@
-// components/LessonLink.tsx
-
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineArrowRight, AiOutlineClockCircle, AiOutlineFlag } from 'react-icons/ai';
-import { TypedLesson, Course } from '~/types/api';
-import { Button } from '~/components/Button';
+import { Lesson } from '~/types/api';
 
 interface LessonLinkProps {
-    course: Course;
-    lesson: TypedLesson;
+    course: { id: number };
+    lesson: Lesson;
 }
 
 export const LessonLink: React.FC<LessonLinkProps> = ({ course, lesson }) => (
     <Link href={`/courses/${course.id}/lesson/${lesson.id}`} passHref>
-        <a className="relative flex flex-col lg:flex-row justify-between duration-200 hover:bg-slate-100 rounded px-4 py-3">
-            <span className="absolute -left-8 top-4 w-6 h-6 bg-slate-200 border-white border-4 rounded-full" />
-            <div>
-                <h5 className="font-medium mb-1">{lesson.number}. {lesson.title}</h5>
-                <div className="text-slate-400">
-                    <span className="inline-flex items-center mr-5">
-                        <AiOutlineClockCircle size={21} className="mr-1" />
-                        {lesson.duration_minutes} min
-                    </span>
-                    <span className="inline-flex items-center">
-                        <AiOutlineFlag size={21} className="mr-1" />
-                        {lesson.points} pts
-                    </span>
-                </div>
-            </div>
-            <span className="flex lg:inline-flex items-center text-deepblue-400">
-                View Lesson <AiOutlineArrowRight size={21} className="ml-1"/>
-            </span>
+        <a className="block p-4 mb-2 bg-white rounded shadow hover:bg-gray-100 transition">
+            <h3 className="text-lg font-semibold">{lesson.number}. {lesson.title}</h3>
+            <p className="text-sm text-gray-500">{lesson.duration_minutes} min | {lesson.points} pts</p>
         </a>
     </Link>
 );
